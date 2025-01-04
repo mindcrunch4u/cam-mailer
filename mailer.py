11953525bb88710b16a6c73d968a0ca73da97c19
mindcrunch4u <peppapigisagoodshow@outlook.com>
2025-01-04 20:52:11 +0800

    refactor

--- mailer.py
+++ mailer.py
@@ -1,4 +1,4 @@
-import email, smtplib
+import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
@@ -6,101 +6,102 @@ from email.mime.image import MIMEImage
 from email import encoders
 from datetime import datetime
 from configuration import default_mail_config, default_misc_config
-import base64
 import os
 
 
-log_file=None
-log_file_path=default_misc_config.path_log_file
+log_file = None
+log_file_path = default_misc_config.path_log_file
+
+
 def log(content):
     global log_file
     global log_file_path
-    if log_file == None:
-        log_file = open(log_file_path, "a+")
-    os.chmod(log_file_path, 0o666)
-    print(content, flush=True)
-    log_file.write("[{}] {} \n".format(datetime.now(), content))
-    log_file.flush()
+    if log_file is None:
+        try:
+            with open(log_file_path, "a+") as log_file:
+                # os.chmod(log_file_path, 0o666)
+                print(content, flush=True)
+                log_file.write("[{}] {} \n".format(datetime.now(), content))
+        except Exception as e:
+            print("Cannot open log, Reason:\n" + str(e))
+            return
+
+
+def create_default_message():
+    mc = default_mail_config
+
+    msg = MIMEMultipart()
+    msg["Subject"] = mc.default_subject
+    msg["From"] = mc.send_from
+    msg["To"] = mc.send_to
+    body = MIMEText("From Your Automated Friend")
+    msg.attach(body)
+
+    return msg, mc
+
+
+def talk_to_mail_server(msg, mc):
+    # parameter: message, mail config
+    service = smtplib.SMTP(mc.smtp_server, mc.smtp_port)
+    service.ehlo()
+    service.starttls()
+    service.ehlo()
+    try:
+        service.login(mc.login_account, mc.login_password)
+    except Exception as e:
+        log("Can't Authenticate, Reason:\n" + str(e))
+    log("Connected to the mail server, starting SEND.")
+    service.sendmail(msg["From"], msg["To"].split(","), msg.as_string())
+
+    result = service.quit()
+    log("Mail sent: {}".format(result))
 
 
 def send_mail_with_images(folder_path, list_of_image_paths):
-        mc = default_mail_config
 
-        if len(list_of_image_paths) <= 0:
-            log("No new images found.")
-            return
+    if len(list_of_image_paths) <= 0:
+        log("No new images found.")
+        return
 
-        msg = MIMEMultipart()
-        msg["Subject"] = mc.default_subject
-        msg["From"] = mc.send_from
-        msg["To"] = mc.send_to
+    msg, mc = create_default_message()
 
-        body = MIMEText("From Your Automated Friend")
-        msg.attach(body)
+    number_of_images = len(list_of_image_paths)
+    for index, path_to_image in enumerate(list_of_image_paths):
+        path_to_image = os.path.join(folder_path, path_to_image)
+        log("\tattaching: {}/{}".format(index + 1, number_of_images))
+        with open(path_to_image, "rb") as f:
+            binary_image = f.read()
+        image = MIMEImage(binary_image, name=os.path.basename(path_to_image))
+        msg.attach(image)
 
-        number_of_images = len(list_of_image_paths)
-        for index, path_to_image in enumerate(list_of_image_paths):
-                path_to_image = os.path.join(folder_path, path_to_image)
-                log("\tattaching: {}/{}".format(
-                        index + 1, number_of_images
-                ))
-                with open(path_to_image, 'rb') as f:
-                        binary_image = f.read()
-                image = MIMEImage(binary_image, name=os.path.basename(path_to_image))
-                msg.attach(image)
+    talk_to_mail_server(msg, mc)
 
-        service = smtplib.SMTP(mc.smtp_server, mc.smtp_port)
-        service.ehlo()
-        service.starttls()
-        service.ehlo()
-        try:
-                service.login(mc.login_account, mc.login_password)
-        except:
-                log("Can't Authenticate")
-        log("Connected to the mail server, starting SEND.")
-        service.sendmail(msg["From"], msg["To"].split(","), msg.as_string())
-
-        result = service.quit()
-        log("Mail sent: {}".format(result))
 
 def send_mail_with_a_video(path_to_video):
-        mc = default_mail_config
+    msg, mc = create_default_message()
 
-        msg = MIMEMultipart()
-        msg["Subject"] = mc.default_subject
-        msg["From"] = mc.send_from
-        msg["To"] = mc.send_to
+    # Attach video file
+    log("Reading file into memory: {}".format(path_to_video))
+    try:
+        with open(path_to_video, "rb") as attachment:
+            attachment_content = attachment.read()
+    except Exception as e:
+        print("Cannot open file: {}, Reason:\n" + str(e))
+        return
+    part = MIMEBase("application", "octet-stream")
+    part.set_payload(attachment_content)
+    encoders.encode_base64(part)
+    part.add_header(
+        "Content-Disposition", "attachment; filename= {}".format(path_to_video)
+    )
+    msg.attach(part)
+    talk_to_mail_server(msg, mc)
 
-        body = MIMEText("From Your Automated Friend")
-        msg.attach(body)
-
-        # Attach video file
-        log("Reading file into memory: {}".format(path_to_video))
-        attachment = open(path_to_video, "rb")
-        part = MIMEBase("application", "octet-stream")
-        part.set_payload(attachment.read())
-        encoders.encode_base64(part)
-        part.add_header("Content-Disposition", "attachment; filename= {}".format(path_to_video))
-        msg.attach(part)
-
-        service = smtplib.SMTP(mc.smtp_server, mc.smtp_port)
-        service.ehlo()
-        service.starttls()
-        service.ehlo()
-        try:
-                service.login(mc.login_account, mc.login_password)
-        except:
-                log("Can't Authenticate")
-        log("Connected to the mail server, starting SEND.")
-        service.sendmail(msg["From"], msg["To"].split(","), msg.as_string())
-
-        result = service.quit()
-        log("Mail sent: {}".format(result))
 
 if __name__ == "__main__":
     import sys
+
     if len(sys.argv) != 2:
         print("usage: python3 script.py <path to a video file>")
         sys.exit(1)
     send_mail_with_a_video(sys.argv[1])
-
--- watchman.py
+++ watchman.py
@@ -1,23 +1,55 @@
-from finder import find_new_files
 from mailer import send_mail_with_images
 import time
-import email, smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
-import base64
 import os
 from configuration import default_misc_config
+import pickle
+
+
+def find_new_files(folder_path):
+    """
+    # Example usage:
+    folder_path = "/var/lib/motion/"
+    new_files, deleted_files = find_new_files(folder_path)
+    """
+    if not os.path.exists(folder_path):
+        raise FileNotFoundError("The folder {} does not exist.".format(folder_path))
+
+    pickle_file_path = os.path.join(folder_path, "file_list.pkl")
+
+    # load the existing file list from the pickle file
+    try:
+        with open(pickle_file_path, "rb") as pickle_file:
+            saved_files = pickle.load(pickle_file)
+    except (FileNotFoundError, pickle.UnpicklingError):
+        saved_files = set()
+
+    # get the current list of JPG files in the folder
+    current_files = set(
+        file
+        for file in os.listdir(folder_path)
+        if file.lower().endswith(default_misc_config.image_extension)
+    )
+
+    # identify new files and deleted files
+    new_files = current_files - saved_files
+    deleted_files = saved_files - current_files
+
+    # update the saved files in the pickle file
+    with open(pickle_file_path, "wb") as pickle_file:
+        pickle.dump(current_files, pickle_file)
+
+    return list(new_files), list(deleted_files)
+
 
 sync_delay = 5
 folder_path = default_misc_config.path_image_folder
 
 while True:
-        try:
-            new_files, deleted_files = find_new_files(folder_path)
-            print("new files:", new_files, flush=True)
-            print("deleted files:", deleted_files, flush=True)
-            send_mail_with_images(folder_path, new_files)
-            time.sleep(sync_delay)
-        except Exception as e:
-            print(e, flush=True)
+    try:
+        new_files, deleted_files = find_new_files(folder_path)
+        print("new files:", new_files, flush=True)
+        print("deleted files:", deleted_files, flush=True)
+        send_mail_with_images(folder_path, new_files)
+        time.sleep(sync_delay)
+    except Exception as e:
+        print(e, flush=True)
